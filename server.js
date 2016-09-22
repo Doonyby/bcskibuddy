8f4523f9978441124c9b9d380537cda15a1b777e
Dan Riding <doonyby@gmail.com>
2016-09-22 14:06:20 -0600

    added intial test

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+
+app.get('/', function (req, res) {
+    res.html('public/index.html');
+    res.status(200);
+});
+
+app.listen(process.env.PORT || 8080, function(err) {
+	if (err) {
+		console.log('port 8080 not working');
+	}
+	else {
+		console.log('listening on port 8080');
+	}
+});
+
+
+exports.app = app;
