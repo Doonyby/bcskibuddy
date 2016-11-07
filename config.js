ea53dac8baf858f484c94bfbd8175e764eff2192
Dan Riding <doonyby@gmail.com>
2016-11-07 14:32:06 -0700

    config space deleted

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 
 						global.DATABASE_URL ||
 						(process.env.NODE_ENV === 'production' ?
-							' mongodb://<doonyby@gmail.com>:<Doonyby21>@ds035177.mlab.com:35177/bcskibuddy' :
+							'mongodb://<doonyby@gmail.com>:<Doonyby21>@ds035177.mlab.com:35177/bcskibuddy' :
 							'mongodb://localhost/bcskibuddy-dev');
 exports.PORT = process.env.PORT || 8080;
