80cff3e518a9bda4c1bb65fb1629ab78fe272534
Dan Riding <doonyby@gmail.com>
2016-11-11 17:29:38 -0700

    posted mongodb_uri

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL || 
 						global.DATABASE_URL ||
 						(process.env.NODE_ENV === 'production' ?
-							'mongodb://<doonyby@gmail.com>:<Doonyby21>@ds035177.mlab.com:35177/bcskibuddy' :
+							'mongodb://heroku_pxdljgjq:7h6vnof1rcnkhrdl136m39hr3g@ds151137.mlab.com:51137/heroku_pxdljgjq' :
 							'mongodb://localhost/bcskibuddy-dev');
 exports.PORT = process.env.PORT || 8080;
